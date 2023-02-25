a5a5c7019eaa39bbe5a76c505b1edcede11d4359
Julia Sukhova <suxova1998@gmail.com>
2023-02-25 17:15:07 +0300

    Задача решена

---
+++ scharp/Homework/Homework_01/Ex008/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int index;
+
+for (index = 1; index <= N; index++ )
+ {
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+    }
+ }
