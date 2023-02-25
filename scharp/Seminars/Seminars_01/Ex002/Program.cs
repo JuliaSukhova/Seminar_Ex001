c0d8bab6cfd81de2ba9d2799fcc25c58ed67149b
Julia Sukhova <suxova1998@gmail.com>
2023-02-25 12:57:55 +0300

    Задача решена

--- scharp/Seminars/Seminars_01/Ex002/Program.cs
+++ scharp/Seminars/Seminars_01/Ex002/Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Задача 2");
 
-
-int N, index;
+int index;
 Console.WriteLine ("Введите число");
-N = (int) inputValue();
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (index = 1; index <= N; index++ )
+ {
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+    }
+ }
