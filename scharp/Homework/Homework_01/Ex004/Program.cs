4e979bbb72be35400329ea5a6c0337f1824fc2dd
Julia Sukhova <suxova1998@gmail.com>
2023-02-25 16:01:07 +0300

    Задача решена

---
+++ scharp/Homework/Homework_01/Ex004/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введте 3 числа");
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ( a >= b && a > c )
+{
+    Console.WriteLine(a);
+} 
+else
+{
+    if (b > a && b >= c)
+    {
+        Console.WriteLine(b);
+    }
+}
+if (c >= a && c > b)
+{
+    Console.WriteLine(c);
+}
