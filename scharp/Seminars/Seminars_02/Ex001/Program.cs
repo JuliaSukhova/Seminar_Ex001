5aacc714697c59d2e9d97b2a793c71bc34a93f3e
Julia Sukhova <suxova1998@gmail.com>
2023-02-28 07:35:08 +0300

    Задача с лекции 2

---
+++ scharp/Seminars/Seminars_02/Ex001/Program.cs
@@ -0,0 +1,20 @@
+﻿string GetNumbers(int count)
+{
+    string output = String.Empty;
+    int index = 1;
+
+    while (index <= count)
+    {
+        if (index % 2 = 0)
+            if (index >= count - 1) output = output + index;
+            else
+                output = output + index + ", ";
+        index++;
+    }
+    return output;
+}
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string res = GetNumbers(N);
+
+Console.WriteLine(res);
