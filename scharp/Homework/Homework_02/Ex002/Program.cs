7663ab18a6a399fde994229db0e6c2693bc354c2
Julia Sukhova <suxova1998@gmail.com>
2023-03-04 12:08:19 +0300

    save

---
+++ scharp/Homework/Homework_02/Ex002/Program.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string massage)
+{
+Console.Write(massage);
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result; 
+}
+
+int GetThirdRank(int n)
+{   
+    while (n > 999)
+    {
+    n /= 10;
+    }
+    return n % 10;
+}
+
+bool ValidateNumber(int n)
+{
+    if (n < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+}
+
+int n = Prompt("Введите число: ");
+if (ValidateNumber(n))
+{
+    Console.WriteLine(GetThirdRank(n));
+}
