78aa74c3fdb18b6e40c90abfec83de319ad7e788
Julia Sukhova <suxova1998@gmail.com>
2023-03-06 14:16:10 +0300

    Завернула метод

--- scharp/Homework/Homework_03/Ex003/Program.cs
+++ scharp/Homework/Homework_03/Ex003/Program.cs
@@ -1,10 +1,22 @@
-﻿int i,a;
+﻿int i= 0;
+int a = 0;
 
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int n = NewMethod();
 
-for (i = 1; i <= n; i++)
+NewMethod1(i, a, n);
+
+static int NewMethod()
 {
-    a = i*i*i;
-    Console.WriteLine(a);
+    Console.WriteLine("Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+static void NewMethod1(int i, int a, int n)
+{
+    for (i = 1; i <= n; i++)
+    {
+        a = i * i * i;
+        Console.WriteLine(a);
+    }
 }
