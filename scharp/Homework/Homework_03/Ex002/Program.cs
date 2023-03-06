017297b8dfdc097aac4073af290a30850ec3b5c6
Julia Sukhova <suxova1998@gmail.com>
2023-03-06 16:02:28 +0300

    Добавлено решение

---
+++ scharp/Homework/Homework_03/Ex002/Program.cs
@@ -0,0 +1,26 @@
+﻿int x1 = Name("x", "A");
+int y1 = Name("y", "A");
+int z1 = Name("z", "A");
+int x2 = Name("x", "B");
+int y2 = Name("y", "B");
+int z2 = Name("z", "B");
+
+int Name(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+double result (double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round(result (x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine(segmentLength);
---
+++ scharp/Lectures/Lectures_02/Ex004/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Aggregate.length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]= new Random() .Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col); // этот метод ничего не возвращает
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int [] array = new int[10]; // создай массив где будет 10 эл.
+
+FillArray(array);
+PrintArray(array);
