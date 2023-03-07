53cd324aa3248dec0c7cdd19295ef9aa87c59433
Julia Sukhova <suxova1998@gmail.com>
2023-03-07 10:21:43 +0300

    new2

---
+++ scharp/Seminars/Seminars_04/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int i = 1;
+int n = 0;
+
+NewMethod();
+
+int A = NewMethod2();
+
+NewMethod1(ref i, ref n, A);
+
+Console.WriteLine(n);
+
+static void NewMethod()
+{
+    Console.Write("Введите число: ");
+}
+
+static void NewMethod1(ref int i, ref int n, int A)
+{
+    while (i <= A)
+    {
+        n = n + i;
+        i++;
+    }
+}
+
+static int NewMethod2()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
