41a49eb656495ff785161e22d6f7c9c2451d7405
Julia Sukhova <suxova1998@gmail.com>
2023-03-09 09:44:18 +0300

    Задача решена

---
+++ scharp/Homework/Homework_04/Ex001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prom(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Nume(int a, int b)
+{
+    int c = 1;
+    while (b != 0)
+    {
+        c = c * a;
+        b = b - 1; 
+    }
+    return c;
+}
+
+int a = Prom("Введите основание: ");
+int b = Prom("Введите показатель: ");
+
+    System.Console.WriteLine($"Число {a} в степень {b} равно {Nume(a,b)})");
