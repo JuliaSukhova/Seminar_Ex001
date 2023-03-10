7c700eef249bc716b8f8deedf28a7136af54a542
Julia Sukhova <suxova1998@gmail.com>
2023-03-10 09:55:11 +0300

    Задача решена

---
+++ scharp/Homework/Homework_04/Ex002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prom(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int SumN(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+int num = Prom("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в цифре {num} = {SumN(num)}");
