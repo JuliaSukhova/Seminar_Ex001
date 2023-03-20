1267e56c20d013e1b3d3c1ebb2e1cde9e2e1ff8a
Julia Sukhova <suxova1998@gmail.com>
2023-03-20 11:21:34 +0300

    Задача решена

---
+++ scharp/Homework/Homework_07/Ex001/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+using System;
+using static System.Console;
+
+Write("Введите количество строк массива: ");
+int info =int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int pillar=int.Parse(ReadLine());
+
+int[,] array = GetArray(info , pillar, -10, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] Noutput)
+{
+    for (int i = 0; i < Noutput.GetLength(0); i++)
+    {
+        for (int j = 0; j < Noutput.GetLength(1); j++)
+        {
+            Write($"{Noutput[i,j]} ");
+        }
+        WriteLine();
+    }
+}
